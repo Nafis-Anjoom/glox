eba4288a7ef5984bee3a8aed4333174c6b49c893
Nafis Anjoom <nafisanjoom@gmail.com>
2024-01-18 15:04:28 -0800

    added main file

--- lox.go
+++ lox.go
@@ -7,8 +7,28 @@ import (
 )
 
 func main() {
-    args := os.Args
-    runFile(args[1])
+    // args := os.Args
+    // runFile(args[1])
+    // types := []string{
+    //     "Binary   : Expr left, Token operator, Expr right",
+    //     "Grouping : Expr expression",
+    //     "Literal  : Object value",
+    //     "Unary    : Token operator, Expr right",
+    // }
+    // tools.DefineAst("./", "expr", types)
+
+    expr := Binary{
+        left: &Unary{
+            operator: Token{MINUS, "-", "", 1},
+            right: &Literal{"123"},
+        },
+        operator: Token{STAR, "*", "", 1},
+        right: &Grouping{
+            expression: &Literal{"45.67"},
+        },
+    }
+
+    fmt.Println(expr.Print())
 }
 
 func runFile(path string) {
