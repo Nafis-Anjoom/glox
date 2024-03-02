5fe4fb1267f9fe9711822122c8cb90bb5919ab94
Nafis Anjoom <nafisanjoom@gmail.com>
2024-03-02 15:19:52 -0800

    improved text representation of Tokens by including token type info

--- lox/token.go
+++ lox/token.go
@@ -4,6 +4,10 @@ import "fmt"
 
 type TokenType int64
 
+func (tokenType TokenType) String() string {
+    return tokeTypeToString[tokenType]
+}
+
 const (
     // Single-character tokens.
     LEFT_PAREN TokenType = iota
@@ -54,6 +58,55 @@ const (
     EOF
 )
 
+var tokeTypeToString = map[TokenType]string {
+    LEFT_PAREN: "LEFT_PAREN",
+    RIGHT_PAREN: "RIGHT_PAREN",
+    LEFT_BRACE: "LEFT_BRACE",
+    RIGHT_BRACE: "RIGHT_BRACE",
+    COMMA: "COMMA",
+    DOT: "DOT",
+    MINUS: "MINUS",
+    PLUS: "PLUS",
+    SEMICOLON: "SEMICOLON",
+    SLASH: "SLASH",
+    STAR: "STAR",
+
+    // One or two character tokens.
+    BANG: "BANG",
+    BANG_EQUAL: "BANG_EQUAL",
+    EQUAL: "EQUAL",
+    EQUAL_EQUAL: "EQUAL_EQUAL",
+    GREATER: "GREATER",
+    GREATER_EQUAL: "GREATER_EQUAL",
+    LESS: "LESS",
+    LESS_EQUAL: "LESS_EQUAL",
+
+    // Literals.
+    IDENTIFIER: "IDENTIFIER",
+    STRING: "STRING",
+    NUMBER: "NUMBER",
+
+    // Keywords.
+    AND: "AND",
+    CLASS: "CLASS",
+    ELSE: "ELSE",
+    FALSE: "FALSE",
+    FUN: "FUN",
+    FOR: "FOR",
+    IF: "IF",
+    NIL: "NIL",
+    OR: "OR",
+    PRINT: "PRINT",
+    RETURN: "RETURN",
+    SUPER: "SUPER",
+    THIS: "THIS",
+    TRUE: "TRUE",
+    VAR: "VAR",
+    WHILE: "WHILE",
+
+    EOF: "EOF",
+}
+
 var keywords = map[string]TokenType{
     "and":    AND,
     "class":  CLASS,
@@ -81,5 +134,5 @@ type Token struct {
 }
 
 func (token Token) String() string {
-    return fmt.Sprintf("%v %v %v %d", token.tokenType, token.lexeme, token.literal, token.line)
+    return fmt.Sprintf("tokenType: %v lexeme: %s\n", token.tokenType, token.lexeme)
 }
