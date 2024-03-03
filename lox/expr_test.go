3fb820b054ddb7a81b9b18f9d9ef76f58f51b48f
Nafis Anjoom <nafisanjoom@gmail.com>
2024-03-02 19:48:24 -0800

    minor cleanup

--- lox/expr_test.go
+++ lox/expr_test.go
@@ -4,13 +4,13 @@ import "testing"
 
 func TestPrint(t *testing.T) {
     expr := Binary{
-        left: &Unary{
+        left: Unary{
             operator: Token{MINUS, "-", "", 1},
-            right: &Literal{"123"},
+            right: Literal{"123"},
         },
         operator: Token{STAR, "*", "", 1},
-        right: &Grouping{
-            expression: &Literal{"45.67"},
+        right: Grouping{
+            expression: Literal{"45.67"},
         },
     }
 
